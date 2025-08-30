ed936d4b6dbcb4788b6d8283ee08ba375d967bc3
YashRajgor <mehtayash@gmail.com>
2025-08-30 18:38:12 +0530

    Loading Process

--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,14 @@
-﻿namespace Hospital_Management_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hospital_Management_System.Models
 {
     public class User
     {
         public int UserId { get; set; }
-        public string? userName { get; set; }
-        public string? password { get; set; }
-        public string? email { get; set; } 
-        public string? phoneNumber { get; set; } 
+        [Required] public string? userName { get; set; }
+        [Required]public string? password { get; set; }
+        [Required] public string? email { get; set; }
+        [Required] public string? phoneNumber { get; set; }
         public int isActive { get; set; }
         public DateTime createdDate { get; set; }
         public DateTime modifiedDate { get; set; }
